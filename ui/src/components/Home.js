341378c1f6581a01cf8a2c6af03e3e3c49d4f9f7
apra-ai <pralex@outlook.de>
2025-04-24 22:17:04 +0200

    create Tabs

--- ui/src/components/Home.js
+++ ui/src/components/Home.js
@@ -1,10 +1,62 @@
 import DrawingApp from ".//DrawingApp"
 
-function Home() {
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <>
-    <DrawingApp/>
-    </>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function Home() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="RAG Document scanner" {...a11yProps(0)} />
+          <Tab label="MNIST number drawing" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Doc scanner
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <DrawingApp></DrawingApp>
+      </CustomTabPanel>
+    </Box>
   );
 }
 
